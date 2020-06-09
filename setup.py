267eb3bee594a5e17f60492040e4ba5e8906e14a
bccheung <bccheung@outlook.com>
2020-06-09 10:27:56 -0400

    Updated setup files and added license

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='wsp-jacquard',
-    version='1.0',
+    version='1.0.0',
+    description='JSON-based configuration handler for models',
+    url='https://github.com/wsp-sag/wsp-jacquard',
+    author='WSP',
+    maintatiner='Brian Cheung',
+    maintainer_email='brian.cheung@wsp.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
     packages=find_packages(),
     install_requires=[
         'six>=1.10'
-    ]
+    ],
+    python_requires='>=2.7'
 )

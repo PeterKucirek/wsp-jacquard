14ab2ca3a23eacf7d14a43dcc1272a198acad96d
Brian Cheung <bccheung@outlook.com>
2022-03-24 11:52:28 -0400

    Update versioneer

--- jacquard/__init__.py
+++ jacquard/__init__.py
@@ -1,6 +1,5 @@
 from .jacquard import Jacquard
 from .api import JacquardParseError, JacquardTypeError, JacquardSpecificationError
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
